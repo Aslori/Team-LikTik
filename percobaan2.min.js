3a54bc05c9fbb03b49bfc558cb6a333162a6da96
Aslori <49122173+Aslori@users.noreply.github.com>
2020-07-14 02:09:25 +0700

    Delete percobaan2.min.js

--- percobaan2.min.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".close-sf").click(function () {
-        $(".Tukang_Redesign_Popup,.Tukang_Redesign_Popup3,#Tukang_Redesign_Header_Container,#wrapper").removeClass("aktif")
-    })
-}), $(document).ready(function () {
-    $(".tmblbtn").click(function () {
-        $(".Tukang_Redesign_Popup,.Tukang_Redesign_Popup3,#Tukang_Redesign_Header_Container,#wrapper").toggleClass("aktif")
-    })
-})
